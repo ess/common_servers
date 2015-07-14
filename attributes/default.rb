c5bd1178daf560432f1c68cfcb559ac807ce2bd2
Dennis Walters <dwalters@engineyard.com>
2015-07-14 15:14:19 -0400

    Exemplified attributes file

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,48 +3,69 @@
 # Attributes: default
 #
 
-default[:common_server] = {
-  # What should we use for client max body size? This affects the maximum
-  # upload file size. Default: 100M
+default[:nginx_common] = {
+  :proxy => {
+    # You MUST set :use_msec_time. If your nginx is version 1.2.7 or
+    # greater, this should be true. Otherwise, it should be false.
+    # :use_msec_time => true,
 
-  :client_max_body_size => '500M',
+    # Should we set a max size for temp files? If so, how many bytes?
+    # :max_temp_file_size => 0,
 
-  :http_white_list => {
-    # Should we reject all HTTP verbs that are not whitelisted?
-    :enabled => true,
+    # Should we set a connect timeout? If so, how many seconds?
+    # :connect_timeout => 300,
 
-    # What verbs should we accept when whitelisting?
-    :accepted_verbs => [
-      'ACL',
-      'BASELINE-CONTROL',
-      'CHECKIN',
-      'CHECKOUT',
-      'CONNECT',
-      'COPY',
-      'DELETE',
-      'GET',
-      'HEAD',
-      'LABEL',
-      'LOCK',
-      'MERGE',
-      'MKACTIVITY',
-      'MKCOL',
-      'MKWORKSPACE',
-      'MOVE',
-      'OPTIONS',
-      'ORDERPATCH',
-      'PATCH',
-      'POST',
-      'PROPFIND',
-      'PROPPATCH',
-      'PUT',
-      'REPORT',
-      'SEARCH',
-      'TRACE',
-      'UNCHECKOUT',
-      'UNLOCK',
-      'UPDATE',
-      'VERSION-CONTROL'
-    ]
+    # Should we set a read timeout? If so, hoa many seconds?
+    # :read_timeout => 300,
+
+    # Should we set a send timeout? If so, how many seconds?
+    # :send_timeout => 300,
+  },
+
+  :servers => {
+    # What should we use for client max body size? This affects the maximum
+    # upload file size. Default: 100M
+
+    # :client_max_body_size => '100M',
+
+    :http_white_list => {
+      # Should we reject all HTTP verbs that are not whitelisted?
+
+      # :enabled => true,
+
+      # What verbs should we accept when whitelisting?
+      #:accepted_verbs => [
+        #'ACL',
+        #'BASELINE-CONTROL',
+        #'CHECKIN',
+        #'CHECKOUT',
+        #'CONNECT',
+        #'COPY',
+        #'DELETE',
+        #'GET',
+        #'HEAD',
+        #'LABEL',
+        #'LOCK',
+        #'MERGE',
+        #'MKACTIVITY',
+        #'MKCOL',
+        #'MKWORKSPACE',
+        #'MOVE',
+        #'OPTIONS',
+        #'ORDERPATCH',
+        #'PATCH',
+        #'POST',
+        #'PROPFIND',
+        #'PROPPATCH',
+        #'PUT',
+        #'REPORT',
+        #'SEARCH',
+        #'TRACE',
+        #'UNCHECKOUT',
+        #'UNLOCK',
+        #'UPDATE',
+        #'VERSION-CONTROL'
+      #],
+    }
   }
 }
